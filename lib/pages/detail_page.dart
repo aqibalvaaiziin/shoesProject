14fc90589150c45b25133d92a2f53dc5e7836d69
aqibalvaaiziin <assddddssaa88>
2020-02-23 09:23:46 +0700

    detail

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:shoes/icons/icon.dart';
-import 'package:shoes/widgets/detail_page/circle_background.dart';
 
 class DetailPage extends StatefulWidget {
   final shoesImage;
@@ -14,184 +13,175 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
-  int counter = 0;
+  var counter = 0;
+  var total = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: Container(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      backgroundColor: Color(0xFFf4f6fb),
+      body: ListView(
+        children: [
+          Transform.translate(
+            offset: Offset(-165, 20),
+            child: CustomIcon.back,
+          ),
+          SizedBox(
+            height:50,
+          ),
+          Center(
+            child: Text(
+              widget.shoesName,
+              style: TextStyle(fontFamily: "NHL2", fontSize: 25),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Stack(
             children: <Widget>[
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: CustomIcon.back,
+              Container(
+                height: MediaQuery.of(context).size.height * 0.8,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.transparent,
+              ),
+              Positioned(
+                top: 75.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60.0),
+                      topRight: Radius.circular(60.0),
+                    ),
+                    color: Colors.white,
+                  ),
+                  height: MediaQuery.of(context).size.height - 120.0,
+                  width: MediaQuery.of(context).size.width,
                 ),
               ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.75,
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Stack(
-                    children: <Widget>[
-                      BackCircle(),
-                      Positioned(
-                        top: 10,
-                        right: 10,
-                        child: Transform.rotate(
-                          angle: -0.3,
-                          child: Image.asset(
-                            widget.shoesImage,
-                            width: 320,
-                          ),
+              Center(
+                child: Container(
+                  width: 250,
+                  height: 250,
+                  decoration: BoxDecoration(
+                      color: Colors.white, shape: BoxShape.circle),
+                ),
+              ),
+              Positioned(
+                top: 160,
+                right: 100,
+                child: Container(
+                  width: 200,
+                  height: 15,
+                  decoration: BoxDecoration(
+                    boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 13)],
+                  ),
+                ),
+              ),
+              Center(
+                child: Hero(
+                  tag: widget.shoesImage,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(widget.shoesImage),
+                            fit: BoxFit.cover)),
+                    height: 250.0,
+                    width: 250.0,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 250.0,
+                left: 25.0,
+                right: 25.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          '\$.' + widget.shoesPrice.toString(),
+                          style: TextStyle(fontSize: 20.0, color: Colors.grey),
                         ),
-                      ),
-                      Positioned(
-                        top: 360,
-                        child: Container(
-                          margin: EdgeInsets.only(left: 20),
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height * 0.5,
+                        Container(
+                          height: 25.0,
+                          color: Colors.grey,
+                          width: 1.0,
+                        ),
+                        Container(
+                          width: 125.0,
+                          height: 40.0,
+                          decoration: BoxDecoration(
+                              color: Color(0xff7a9bee),
+                              borderRadius: BorderRadius.circular(11.0)),
                           child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: <Widget>[
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Container(
-                                    width:
-                                        MediaQuery.of(context).size.width * .6,
-                                    child: Text(
-                                      widget.shoesName,
-                                      style: TextStyle(
-                                        fontSize: 25,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 7,
-                                  ),
-                                  Text(
-                                    "Rp. " + widget.shoesPrice.toString(),
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.grey),
-                                  ),
-                                ],
-                              ),
-                              Transform.translate(
-                                offset: Offset(-40, -195),
+                              InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    if (counter <= 0) {
+                                      counter = 0;
+                                    } else {
+                                      counter -= 1;
+                                      total -= int.parse(
+                                          widget.shoesPrice.toString());
+                                    }
+                                  });
+                                },
                                 child: Container(
-                                  width: MediaQuery.of(context).size.width * .4,
-                                  height: 50,
+                                  height: 25.0,
+                                  width: 25.0,
                                   decoration: BoxDecoration(
-                                    color: Colors.black.withOpacity(.7),
-                                    borderRadius: BorderRadius.all(
-                                      Radius.circular(8),
-                                    ),
-                                    boxShadow: [
-                                      BoxShadow(
-                                          color: Colors.grey,
-                                          blurRadius: 10,
-                                          offset: Offset(0, 6))
-                                    ],
+                                    borderRadius: BorderRadius.circular(7.0),
+                                    color: Colors.white,
                                   ),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceAround,
-                                    children: <Widget>[
-                                      InkWell(
-                                        onTap: () {
-                                          if (counter <= 0)
-                                            counter = 0;
-                                          else
-                                            setState(() {
-                                              counter -= 1;
-                                            });
-                                        },
-                                        child: Text(
-                                          "-",
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 35,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                      ),
-                                      Text(
-                                        counter.toString(),
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      InkWell(
-                                        onTap: () {
-                                          setState(() {
-                                            counter += 1;
-                                          });
-                                        },
-                                        child: Text(
-                                          "+",
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 30,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                      ),
-                                    ],
+                                  child: Icon(
+                                    Icons.remove,
+                                    color: Color(0xff7a9bee),
+                                    size: 20.0,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                counter.toString(),
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 15.0),
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    counter += 1;
+                                    total +=
+                                        int.parse(widget.shoesPrice.toString());
+                                  });
+                                },
+                                child: Container(
+                                  height: 25.0,
+                                  width: 25.0,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(7.0),
+                                    color: Colors.white,
+                                  ),
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Color(0xff7a9bee),
+                                    size: 20.0,
                                   ),
                                 ),
                               ),
                             ],
                           ),
-                        ),
-                      ),
-                      Transform.translate(
-                        offset: Offset(20, 460),
-                        child: Container(
-                            margin: EdgeInsets.only(left: 20),
-                            width: MediaQuery.of(context).size.width * 0.8,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  widget.shoesName,
-                                  style: TextStyle(
-                                      fontSize: 40,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                SizedBox(
-                                  height: 15,
-                                ),
-                                Center(
-                                  child: Text(
-                                    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore repudiandae sunt incidunt aliquam. Facilis, fugit ipsa! Omnis soluta consectetur placeat quod nisi, totam eaque aliquid magni fugiat earum dolorum quo?",
-                                    style: TextStyle(
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.black38),
-                                  ),
-                                )
-                              ],
-                            )),
-                      )
-                    ],
-                  ),
+                        )
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
