263505b131fc0bd8131004d5fcd12eb5b8c3cb83
aqibalvaaiziin <aqibalvaaiziin@gmail.com>
2020-03-28 20:01:06 +0700

    search retail done

--- lib/pages/retail_page.dart
+++ lib/pages/retail_page.dart
@@ -9,14 +9,15 @@ class RetailPage extends StatefulWidget {
 
 class _RetailPageState extends State<RetailPage> {
   TextEditingController search = TextEditingController();
-  List<RetailFactory> result = new List();
-  List<RetailFactory> searchResult = new List();
+  var result = List<RetailFactory>();
+  var temp = List<RetailFactory>();
 
   void dataRetail() {
     RetailFactory.getRetail().then((value) {
       for (var i = 0; i < value.length; i++) {
         setState(() {
           result.add(value[i]);
+          temp.add(value[i]);
         });
       }
     });
@@ -24,8 +25,32 @@ class _RetailPageState extends State<RetailPage> {
 
   @override
   void initState() {
-    super.initState();
     dataRetail();
+    super.initState();
+  }
+
+  void onSearchTextChanged(String text) async {
+    List<RetailFactory> searchResult = List<RetailFactory>();
+    searchResult.addAll(temp);
+    if (search != null || search != "") {
+      List<RetailFactory> dummy = List<RetailFactory>();
+      searchResult.forEach((item) {
+        if (item.nama.toLowerCase().contains(text) ||
+            item.lokasi.toLowerCase().contains(text)) {
+          dummy.add(item);
+        }
+      });
+      setState(() {
+        result.clear();
+        result.addAll(dummy);
+      });
+      return;
+    } else {
+      setState(() {
+        result.clear();
+        dataRetail();
+      });
+    }
   }
 
   @override
@@ -70,7 +95,9 @@ class _RetailPageState extends State<RetailPage> {
               ),
               child: TextField(
                 controller: search,
-                onChanged: onSearchTextChanged,
+                onChanged: (value) {
+                  onSearchTextChanged(value);
+                },
                 decoration: InputDecoration(
                     border: InputBorder.none,
                     suffixIcon: CustomIcon.search,
@@ -79,154 +106,70 @@ class _RetailPageState extends State<RetailPage> {
             ),
           ),
           Expanded(
-            child: searchResult.length != 0 || search.text.isNotEmpty
-                ? ListView.builder(
-                    itemCount: searchResult.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        margin: EdgeInsets.fromLTRB(25, 0, 25, 30),
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        width: MediaQuery.of(context).size.width * 0.8,
-                        height: 80,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.grey[300],
-                                blurRadius: 8,
-                                spreadRadius: 2,
-                                offset: Offset(5, 8)),
-                          ],
-                        ),
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                              Icons.store_mall_directory,
-                              size: 70,
+              child: ListView.builder(
+            itemCount: result.length,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: EdgeInsets.fromLTRB(25, 0, 25, 30),
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey[300],
+                        blurRadius: 8,
+                        spreadRadius: 2,
+                        offset: Offset(5, 8)),
+                  ],
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.store_mall_directory,
+                      size: 70,
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            result[index].nama,
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontFamily: "D",
+                              fontWeight: FontWeight.bold,
                             ),
-                            Container(
-                              padding: EdgeInsets.symmetric(vertical: 10),
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    result[index].nama,
-                                    style: TextStyle(
-                                      fontSize: 17,
-                                      fontFamily: "D",
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    result[index].telp,
-                                    style: TextStyle(
-                                        fontSize: 14,
-                                        fontFamily: "FL",
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                  Text(
-                                    result[index].lokasi,
-                                    style: TextStyle(
-                                      fontFamily: "FL",
-                                      fontSize: 13,
-                                      color: Colors.grey,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      );
-                    },
-                  )
-                : ListView.builder(
-                    itemCount: result.length,
-                    itemBuilder: (context, i) {                      
-                      return Container(
-                        margin: EdgeInsets.fromLTRB(25, 0, 25, 30),
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        width: MediaQuery.of(context).size.width * 0.8,
-                        height: 80,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.grey[300],
-                                blurRadius: 8,
-                                spreadRadius: 2,
-                                offset: Offset(5, 8)),
-                          ],
-                        ),
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                              Icons.store_mall_directory,
-                              size: 70,
+                          ),
+                          Text(
+                            result[index].telp,
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontFamily: "FL",
+                                fontWeight: FontWeight.w600),
+                          ),
+                          Text(
+                            result[index].lokasi,
+                            style: TextStyle(
+                              fontFamily: "FL",
+                              fontSize: 13,
+                              color: Colors.grey,
                             ),
-                            Container(
-                              padding: EdgeInsets.symmetric(vertical: 10),
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    result[i].nama,
-                                    style: TextStyle(
-                                      fontSize: 17,
-                                      fontFamily: "D",
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    result[i].telp,
-                                    style: TextStyle(
-                                        fontSize: 14,
-                                        fontFamily: "FL",
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                  Text(
-                                    result[i].lokasi,
-                                    style: TextStyle(
-                                      fontFamily: "FL",
-                                      fontSize: 13,
-                                      color: Colors.grey,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-          ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          )),
         ],
       ),
     );
-  }
-
-  onSearchTextChanged(String text) async {
-    searchResult.clear();
-    if (text.isEmpty) {
-      setState(() {});
-      return;
-    }
-
-    result.forEach((data){
-      if (data.nama.toLowerCase().contains(text.toLowerCase()) ||
-          data.lokasi.toLowerCase().contains(text.toLowerCase())) {
-        searchResult.add(data);
-        print(data.nama.toLowerCase());
-      }
-    });
-
-    setState(() {});
   }
 }
