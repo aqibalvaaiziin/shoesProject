8ad7db23c5487f43455b9dc6cb889b050c19d328
aqibalvaaiziin <aqibalvaaiziin@gmail.com>
2020-03-29 11:27:28 +0700

    try editprofile factory

--- lib/factory/edit_profile_factory.dart
+++ lib/factory/edit_profile_factory.dart
@@ -1,5 +1,36 @@
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
 
-class EditProfileFactory{
-  
+class EditProfileFactory {
+  String message;
+
+  EditProfileFactory({this.message});
+
+  factory EditProfileFactory.getResult(String object) {
+    return EditProfileFactory(message: object);
+  }
+
+  static Future<EditProfileFactory> setRequest(
+    String id,
+    String nama,
+    String email,
+    String password,
+    String alamat,
+    String telp,
+    String foto,
+  ) async {
+    var apiUrl = Uri.parse("https://sepatu.gopla.xyz/user/" + id);
+    var requestData = http.MultipartRequest('PUT', apiUrl)
+      ..fields['nama'] = nama
+      ..fields['email'] = email
+      ..fields['password'] = password
+      ..fields['alamat'] = alamat
+      ..fields['telp'] = telp
+      ..files.add(await http.MultipartFile.fromPath('foto', foto));
+
+    var response = await requestData.send();
+    if (response.statusCode == 200)
+      return EditProfileFactory.getResult("data upadated");
+  }
 }
